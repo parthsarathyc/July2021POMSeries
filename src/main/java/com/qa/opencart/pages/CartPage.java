38fc7b525330aeae1e303e8a2d7ae982f40b5d92
Parth <parthsarathyc@gmail.com>
2021-11-08 19:48:31 +0530

    updated cart page

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -4,7 +4,10 @@ public class CartPage {
 	
 	public CartPage() {
 		System.out.println("cart page");
+		System.out.println("review coments added ");
 	}
+	
+	
 	
 	public void addToCart() {
 		System.out.println("add to cart");
