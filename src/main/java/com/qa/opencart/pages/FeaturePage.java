e38ab188e2d67ca12201c5d86048828203f040a3
Parth <parthsarathyc@gmail.com>
2021-11-10 14:46:42 +0530

    f2 added

--- src/main/java/com/qa/opencart/pages/FeaturePage.java
+++ src/main/java/com/qa/opencart/pages/FeaturePage.java
@@ -3,7 +3,10 @@ package com.qa.opencart.pages;
 public class FeaturePage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		
+		System.out.println("f1");
+		System.out.println("f2");
 
 	}
 
