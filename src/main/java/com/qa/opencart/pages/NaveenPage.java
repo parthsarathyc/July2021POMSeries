da628bc344bc686643b542eb5afd98a800dcf2dd
Parth <parthsarathyc@gmail.com>
2021-11-11 15:30:05 +0530

    naveen feature completed

--- src/main/java/com/qa/opencart/pages/NaveenPage.java
+++ src/main/java/com/qa/opencart/pages/NaveenPage.java
@@ -5,8 +5,10 @@ public class NaveenPage {
 	public static void main(String[] args) {
 		
 		System.out.println("demo 1");
-		
-			System.out.println("demo 2");
+
+		System.out.println("demo 3 by naveen");
+		System.out.println("demo 4 ");
+
 		
 		
 		
