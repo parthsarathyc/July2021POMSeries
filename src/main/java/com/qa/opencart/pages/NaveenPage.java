85eaae4cf62e1345983a2559da5d3ee2ef8647ed
Parth <parthsarathyc@gmail.com>
2021-11-11 15:04:24 +0530

    demo 11 by naveen

--- src/main/java/com/qa/opencart/pages/NaveenPage.java
+++ src/main/java/com/qa/opencart/pages/NaveenPage.java
@@ -3,8 +3,13 @@ package com.qa.opencart.pages;
 public class NaveenPage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("demo 1");
+		
+		
+		
+		
+		
 	}
 
 }
